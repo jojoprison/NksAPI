cf0009eb4c85e1274aa3b2b269bcf1f5da92da8a
Egor [home] Zubachenko <egyabig2@gmail.com>
2022-03-25 00:09:12 +0300

    add views for filters

--- CatalogueApp/views.py
+++ CatalogueApp/views.py
@@ -1,4 +1,6 @@
 import json
+import operator
+from functools import reduce
 
 from django.db.models import Q
 from django.shortcuts import render
@@ -55,23 +57,77 @@ def product_api(request, product_id=0):
 @csrf_exempt
 def product_filter_all_api(request):
     if request.method == 'GET':
-        temp_filter_list = list(set(Product.objects.all().values_list('photo_file_name', flat=True)))
-        temp_filter_list.sort()
+        photo_list = list(set(Product.objects.all().values_list('photo_file_name', flat=True)))
+        photo_list.sort()
+        print(photo_list)
 
-        return JsonResponse(temp_filter_list, safe=False)
+        price_list = list(set(Product.objects.all().exclude(price=None).values_list('price', flat=True)))
+        print(price_list)
+        price_list.sort()
+
+        type_list = list(set(Product.objects.all().values_list('type__title', flat=True)))
+        type_list.sort()
+
+        filter_variant_list = {
+            'select': [
+                {'product_prop': 'type',
+                 # TODO мб как то можно verbose_name засунуть из модельки сюда
+                 'name': 'Тип',
+                 'values': type_list},
+                {'product_prop': 'price',
+                 'name': 'Цена',
+                 'values': price_list},
+                {'product_prop': 'photo_file_name',
+                 'name': 'Фото',
+                 'values': photo_list},
+                {'product_prop': 'width',
+                 'name': 'Ширина',
+                 'values': photo_list},
+                {'product_prop': 'photo_file_name',
+                 'name': 'Фото',
+                 'values': photo_list},
+                {'product_prop': 'photo_file_name',
+                 'name': 'Фото',
+                 'values': photo_list},
+                {'product_prop': 'photo_file_name',
+                 'name': 'Фото',
+                 'values': photo_list},
+            ],
+            'checkbox': ['is_published', 'lol', 'temp']
+        }
+
+        print(filter_variant_list)
+
+        return JsonResponse(filter_variant_list, safe=False)
 
 
 @csrf_exempt
 def product_filter_api(request):
     if request.method == 'GET':
-        photo = request.GET.get('photo', None)
+        filter_kwargs = []
 
-        if photo == 'ALL':
+        # qs = Q()
+        # for query in queries:
+        #     qs = qs | query
+
+        for item in request.GET.items():
+            # TODO продумать как для related полей делать проверку чтоб добавлять title к ним
+            if item[0] == 'type':
+                filter_kwargs.append(Q(**{f'{item[0]}__title': item[1]}))
+            else:
+                filter_kwargs.append(Q(**{item[0]: item[1]}))
+
+        print(filter_kwargs)
+
+        # TODO заменить проверки вот тут
+        if request.GET.get('photo', None) == 'ALL':
             prods_by_photo = Product.objects.all()
         else:
-            prods_by_photo = Product.objects.filter(photo_file_name=photo)
+            if filter_kwargs:
+                prods_by_photo = Product.objects.filter(reduce(lambda a, b: a & b, filter_kwargs))
+            else:
+                prods_by_photo = Product.objects.all()
 
-        print(prods_by_photo)
         product_serializer = ProductSerializer(prods_by_photo, many=True)
 
         return JsonResponse(product_serializer.data, safe=False)
@@ -139,6 +195,7 @@ def order_api(request):
 
         if order_client_phone or order_client_email:
             client = Client.objects.filter(Q(phone=order_client_phone) | Q(email=order_client_email)).first()
+            print(Client.objects.filter(Q(phone=order_client_phone) | Q(email=order_client_email)).query)
 
         order_data['client'] = client.id
         order_data['products'] = json.dumps(order_data.get('products'))
