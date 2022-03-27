33d8fe92da1c0fca2edd39fe7c3d835a940a2828
Egor [home] Zubachenko <egyabig2@gmail.com>
2022-03-28 00:17:23 +0300

    change position

--- NksAPI/settings.py
+++ NksAPI/settings.py
@@ -19,8 +19,9 @@ import os
 # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # another old...
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = Path(__file__).resolve().parent.parent
+# incorrect
 # BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
