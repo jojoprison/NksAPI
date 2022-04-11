d3d490909f08adb721505f1c5b1a285c69d1758a
Egor [home] Zubachenko <egyabig2@gmail.com>
2022-04-11 14:59:00 +0300

    update phone list

--- common/utils/whatsapp_notification.py
+++ common/utils/whatsapp_notification.py
@@ -10,23 +10,27 @@ class WhatsAppNotificator:
         config = configparser.ConfigParser()
         config.read(f'{get_project_root_path()}/config.ini')
         db_config = config['whatsapp']
+        self.recipients = ({'phone': db_config['phone_nks_1'], 'token': db_config['token_nks_1']},
+                           {'phone': db_config['phone_nks_2'], 'token': db_config['token_nks_2']},
+                           {'phone': db_config['phone_nks_3'], 'token': db_config['token_nks_3']})
 
         # phone_qdf phone_eug
-        self.phone = db_config['phone_nks_1']
+        # self.phone = db_config['phone_nks_1']
         # token_qdf token_eug
-        self.token = db_config['token_nks_1']
+        # self.token = db_config['token_nks_1']
 
     def send_message(self, text):
-        payload = {
-            'phone': self.phone,
-            'token': self.token,
-            'text': text
-        }
+        for recipient in self.recipients:
+            payload = {
+                'phone': recipient['phone'],
+                'token': recipient['token'],
+                'text': text
+            }
 
-        url = 'https://whin.inutil.info/whin'
+            url = 'https://whin.inutil.info/whin'
 
-        res = requests.post(url, data=payload)
-        print(res.text)
+            res = requests.post(url, data=payload)
+            print(res.text)
 
 
 if __name__ == '__main__':
