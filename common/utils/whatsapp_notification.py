15e3aa78eb4f0591a6f20d50a6fa7a0f3eb26d9d
jkaseq <egyabig2@gmail.com>
2023-02-03 19:35:09 +0300

    Update whatsapp_notification.py

--- common/utils/whatsapp_notification.py
+++ common/utils/whatsapp_notification.py
@@ -10,25 +10,28 @@ class WhatsAppNotificator:
         config = configparser.ConfigParser()
         config.read(f'{get_project_root_path()}/config.ini')
         db_config = config['whatsapp']
-        self.recipients = ({'phone': db_config['phone_nks_1'], 'token': db_config['token_nks_1']},
-                           {'phone': db_config['phone_nks_2'], 'token': db_config['token_nks_2']},
-                           {'phone': db_config['phone_nks_3'], 'token': db_config['token_nks_3']},
-                           {'phone': db_config['phone_qdf'], 'token': db_config['token_qdf']})
+        self.recipients = ({'phone': db_config['phone_nks'], 'token': db_config['token_nks']})
+                           # {'phone': db_config['phone_nks_2'], 'token': db_config['token_nks_2']},
+                           # {'phone': db_config['phone_nks_3'], 'token': db_config['token_nks_3']},
+                           # {'phone': db_config['phone_qdf'], 'token': db_config['token_qdf']})
 
         # self.phone = db_config['phone_nks_1']
         # self.token = db_config['token_nks_1']
 
     def send_message(self, text):
-        for recipient in self.recipients:
-            payload = {
-                'phone': recipient['phone'],
-                'token': recipient['token'],
-                'text': text
-            }
+        # for recipient in self.recipients:
+        config = configparser.ConfigParser()
+        config.read(f'{get_project_root_path()}/config.ini')
+        db_config = config['whatsapp']
+        payload = {
+            'phone': db_config['phone_nks'],
+            'token': db_config['token_nks'],
+            'text': text
+        }
 
-            url = 'https://whin.inutil.info/whin'
+        url = 'https://whin.inutil.info/whin'
 
-            requests.post(url, data=payload)
+        requests.post(url, data=payload)
 
 
 if __name__ == '__main__':
